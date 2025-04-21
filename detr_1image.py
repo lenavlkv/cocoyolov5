a26893aff80864cdd870f2f2e222b0bb270f4c64
lenavlkv <lenavlkv@mail.ru>
2025-04-21 11:08:16 +0300

    detr

---
+++ detr_1image.py
@@ -0,0 +1,239 @@
+import argparse
+import os
+import tempfile
+import torch
+import torchvision.ops as ops
+import json
+import warnings
+from collections import defaultdict
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+from torchvision.datasets import CocoDetection
+from torchvision.transforms import ToTensor, Compose
+from tqdm import tqdm
+from PIL import Image
+import io
+import pathlib
+from collections import defaultdict
+from typing import Any, Callable, Dict, Iterable, List, Optional, Set, Tuple, Union
+import torch.nn as nn
+import numpy as np
+from torchvision.ops import box_convert
+
+
+
+warnings.filterwarnings(action="ignore")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser("Evaluate MS COCO mAP")
+    parser.add_argument("--data", help="Path to the dataset", default=".")
+    parser.add_argument("--split", help="Dataset part", default="val2017")
+    parser.add_argument("--batch-size", help="Batch size", type=int, default=32)
+    parser.add_argument("--image-size", help="Image size", type=int, default=640)
+    parser.add_argument("--num-workers", help="Image size", type=int, default=8)
+    parser.add_argument("--iou-threshold", help="Evaluation IoU threshold", type=float, default=0.5)
+    parser.add_argument("--dump-predictions", help="Dump evaluation results to a json file with a given name")
+    parser.add_argument("--top-k", type=int, default=1, help="Predict top K labels for each bbox")
+    return parser.parse_args()
+
+
+class Resize:
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, image):
+        w, h = image.size
+        scale = self.size / max(w, h)
+        return image.resize((int(round(w * scale)), int(round(h * scale))),
+                            resample=Image.BILINEAR)
+
+
+class AddPadding:
+    def __init__(self, pad_value=114 / 255.0):
+        self.pad_value = pad_value
+
+    def __call__(self, image, targets):
+        c, h, w = image.shape
+        size = max(h, w)
+        result = torch.full((c, size, size), self.pad_value, dtype=torch.float, device=image.device)
+        top = (size - h) // 2
+        left = (size - w) // 2
+        result[:, top:top + h, left:left + w] = image
+        new_targets = []
+        for t in targets:
+            bx, by, bw, bh = t["bbox"]
+            new_targets.append({
+                "image_id": t["image_id"],
+                "bbox": (bx + left, by + top, bw, bh),
+                "category_id": t["category_id"]
+            })
+        return top, left, result, new_targets
+
+
+class Transform:
+    def __init__(self, image_size):
+        self.image_size = image_size
+        self.transform = Compose([
+            Resize(image_size),
+            ToTensor()
+        ])
+        self.padding = AddPadding()
+
+    def __call__(self, image, targets):
+        orig_w, orig_h = image.size
+        image = self.transform(image)
+        top, left, image, targets = self.padding(image, targets)
+        image_id = targets[0]["image_id"] if targets else None
+        for t in targets:
+            assert t["image_id"] == image_id
+        targets = {
+            "image_id": image_id,
+            "offset": (top, left),
+            "scale": image.shape[1] / max(orig_w, orig_h),
+            "detections": [{"bbox": t["bbox"],
+                            "category_id": t["category_id"]}
+                           for t in targets]
+        }
+        return image, targets
+
+
+def collate_fn(batch):
+    images, targets = zip(*batch)
+    return torch.stack(images, 0), targets
+
+
+def result2coco(anno, results, dump_predictions=None):
+    clean = []
+    for r in results:
+        clean.append({
+            "image_id": r["image_id"],
+            "bbox": r["bbox"],
+            "category_id": r["category_id"]
+        })
+        if "score" in r:
+            clean[-1]["score"] = r["score"]
+    if dump_predictions:
+        with open(dump_predictions, "w") as fp:
+            json.dump(clean, fp)
+        return anno.loadRes(dump_predictions)
+    else:
+        with tempfile.NamedTemporaryFile(mode="w", delete=False) as fp:
+            json.dump(clean, fp)
+            fp.flush()
+            return anno.loadRes(fp.name)
+
+
+
+def evaluate(model, data_root, data_split,
+             batch_size, image_size,
+             iou_threshold,
+             num_workers,
+             dump_predictions=None,
+             min_confidence=0.001,
+             topk=1,
+             target_image_id=3553):
+    use_cuda = torch.cuda.is_available()
+
+    transforms = Transform(image_size)
+    dataset = CocoDetection(os.path.join(data_root, data_split),
+                            os.path.join(data_root, "annotations", f"instances_{data_split}.json"),
+                            transforms=transforms)
+
+    anno = COCO(os.path.join(data_root, "annotations", f"instances_{data_split}.json"))
+    loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                         pin_memory=use_cuda, num_workers=num_workers,
+                                         collate_fn=collate_fn)
+
+    model.eval()
+    if use_cuda:
+        model.cuda()
+    print(f"The dataset contains {len(dataset)} images.")
+
+    results = []
+    with torch.no_grad():
+        for batch_idx, (images, targets) in enumerate(tqdm(loader)):
+            if use_cuda:
+                images = images.cuda()
+            predictions = model(images)
+            pred_logits = predictions['pred_logits']
+            pred_boxes = predictions['pred_boxes']
+
+            bboxes = pred_boxes.clone()
+            #bboxes = bboxes*640
+            bboxes[..., :2] -= bboxes[..., 2:] / 2
+
+            prob = nn.functional.softmax(pred_logits, -1)
+            scores, labels = prob[..., :-1].max(-1)
+
+            predictions_mask = scores >= min_confidence
+
+            for i in range(len(images)):
+                image_id = dataset.ids[batch_idx * batch_size + i]
+
+                if image_id != target_image_id:
+                    continue
+
+                print(f"\nProcessing image ID: {image_id}")
+
+                ann_ids = anno.getAnnIds(imgIds=image_id)
+                gt_anns = anno.loadAnns(ann_ids)
+                gt_bboxes = [ann["bbox"] for ann in gt_anns]
+                gt_cats = [ann["category_id"] for ann in gt_anns]
+
+                print("Ground Truth:")
+                for bbox, cat_id in zip(gt_bboxes, gt_cats):
+                    print(f"  Category: {cat_id}, BBox: {bbox}")
+
+                # Обработка предсказаний
+                if targets[i]["image_id"] is not None:
+                    assert targets[i]["image_id"] == image_id
+                scale = targets[i]["scale"]
+                top, left = targets[i]["offset"]
+                offset = torch.tensor((left, top, 0, 0), device=images.device)
+
+                padded_size = images[i].shape[-1] # ==640
+
+                image_mask = predictions_mask[i] >= min_confidence
+                image_bboxes = bboxes[i][image_mask]#*padded_size #потому что detr возвращает координаты bboxes в нормализованном виде (от 0 до 1)
+                image_confidences = scores[i][image_mask]
+                image_label_probs = labels[i][image_mask]
+
+                image_bboxes_xyxy = torch.cat([image_bboxes[:, :2], image_bboxes[:, :2] + image_bboxes[:, 2:]], 1)
+                image_bboxes_xyxy = image_bboxes_xyxy*640
+                nms_indices = ops.nms(image_bboxes_xyxy, image_confidences, iou_threshold)
+                bboxes_cpu = image_bboxes[nms_indices].cpu()
+                label_confidences_cpu = (
+                        image_label_probs[nms_indices] * image_confidences[nms_indices][:, None]).cpu()
+                offset_cpu = offset.cpu()
+                scale_cpu = torch.tensor(scale, device='cpu')
+
+                for j in range(len(nms_indices)):
+                    top_scores, top_categories = label_confidences_cpu[j].topk(topk)
+
+                    for score, category in zip(top_scores, top_categories):
+                        results.append({
+                                "image_id": image_id,
+                                "bbox": ((bboxes_cpu[j] - offset_cpu) / scale_cpu).tolist(),
+                                "category_id": category.item(),
+                                "score": score.item(),
+                                "label_probs": label_confidences_cpu[j]
+                           })
+                print(results)
+                return
+
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    model = torch.hub.load('facebookresearch/detr', 'detr_resnet50', pretrained=True)
+
+    # Вызываем evaluate с указанием целевого image_id
+    evaluate(model, args.data, args.split,
+             batch_size=args.batch_size,
+             image_size=args.image_size,
+             iou_threshold=args.iou_threshold,
+             num_workers=args.num_workers,
+             dump_predictions=args.dump_predictions,
+             topk=args.top_k,
+             target_image_id=3553)
